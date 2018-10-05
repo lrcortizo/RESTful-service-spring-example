8a16f7bf64026110cc59764bf8afd3b778e72938
Luis Raña <luisraco95@gmail.com>
2018-10-05 11:07:26 +0200

    Oder entity, controller and repository

--- demo/src/main/java/com/example/demo/LoadDatabase.java
+++ demo/src/main/java/com/example/demo/LoadDatabase.java
@@ -11,10 +11,16 @@ import org.springframework.context.annotation.Configuration;
 class LoadDatabase {
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LoadDatabase.class);
 	@Bean
-	CommandLineRunner initDatabase(EmployeeRepository repository) {
+	CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository orderRepository) {
 		return args -> {
 			log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
 			log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+			log.info("Preloading " + orderRepository.save(new Order("MacBook Pro", Order.Status.COMPLETED)));
+			log.info("Preloading " + orderRepository.save(new Order("iPhone", Order.Status.IN_PROGRESS)));
 		};
+		
+		orderRepository.findAll().forEach(order -> {
+			log.info("Preloaded " + order);
+		});
 	}
 }
